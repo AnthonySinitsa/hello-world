2fcfab043ca11c2ac05ca6c94106dd21b5518e73
AnthonySinitsa <itonyemail@gmail.com>
2023-03-23 00:16:50 +0000

    added js

---
+++ script.js
@@ -0,0 +1,14 @@
+//let
+//const
+//four egg sample
+//let usersName = prompt("what isname?");
+
+let myName = "Anthony";
+document.write("My name is " + myName + " the creator. ");
+
+let userName = prompt("Your name: ");
+document.write("Welcome to my page " + userName + "! ");
+
+if (userName == "Kassie") {
+  document.write(":D Good day!");
+}
