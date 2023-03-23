af96c5215569d643a1dfbde3f2cf89861b4c4c94
AnthonySinitsa <itonyemail@gmail.com>
2023-03-23 20:02:01 +0000

    added functions

--- script.js
+++ script.js
@@ -3,12 +3,35 @@
 //four egg sample
 //let usersName = prompt("what isname?");
 
-let myName = "Anthony";
-document.write("My name is " + myName + " the creator. ");
+document.write("My name is Anthony the creator. ");
 
-let userName = prompt("Your name: ");
-document.write("Welcome to my page " + userName + "! ");
+function inputUser() {
+  let userName = prompt("Your name: ");
 
-if (userName == "Kassie") {
-  document.write(":D Good day!");
+  if (userName == "") {
+    userName = prompt("Actually put in your name. ");
+  } else if ((userName == "John") | (userName == "john")) {
+    document.write("Welcome Boss! ");
+  } else {
+    document.write("Welcome to my page " + userName + "! ");
+  }
+  return userName;
 }
+
+function guessNumber() {
+  let inputNumber = prompt("guess a number between 0-9: ");
+
+  if (inputNumber != 5) {
+    document.write("wrong number");
+  } else {
+    document.write("correct number");
+  }
+  return inputNumber;
+}
+
+// let theUsersName = inputUser();
+// document.write(theUsersName);
+
+//structure of function
+//function DECLARATION
+//function addTwoNumbers(parameters){code to execute}
